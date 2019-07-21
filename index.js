b3383746d611e90f327be9a65e65c1653c87f68a
aa1aac <ach.aayush.111@gmail.com>
2019-07-21 22:58:21 +0545

    authentication flow with google-Oauth

--- index.js
+++ index.js
@@ -1,9 +1,30 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const cookieSession = require("cookie-session");
+const passport = require("passport");
+
+const keys = require("./config/keys");
+
+require("./models/User"); // user model mongoose
+
+require("./services/passport"); //passport middleware
+
+mongoose.connect(keys.mongoURI, { useNewUrlParser: true });
+
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send({ hi: "there" });
-});
+app.use(
+  cookieSession({
+    maxAge: 30 * 24 * 60 * 60 * 1000,
+    keys: [keys.cookieKey]
+  })
+);
 
-const PORT = process.env.PORT || 3000;
+app.use(passport.initialize());
+app.use(passport.session());
+
+require("./routes/authRoutes")(app); // for routing authRoutes
+
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => console.log("deployed"));
+//42
