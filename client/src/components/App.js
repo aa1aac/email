eababa0b531f38947526775e9d886de783d02a6b
aa1aac <ach.aayush.111@gmail.com>
2019-08-15 22:57:36 +0545

    basic implementation for route protection

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,16 +1,31 @@
 import React, { Component } from "react";
-import { BrowserRouter, Route } from "react-router-dom";
+import { BrowserRouter, Route, Redirect } from "react-router-dom";
 import { connect } from "react-redux";
+import axios from "axios";
 import * as actions from "../actions";
 
 import Header from "./Header";
 import Landing from "./Landing";
 import Dashboard from "./Dashboard";
 import NewMail from "./NewMail";
+import AuthExample from "./protectedroutePractice";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { isLoggedin: false };
+  }
+
   componentDidMount() {
     this.props.fetchUser();
+    console.log(this.props.fetchUser());
+    axios.get("/api/current_user").then(res => {
+      if (res.data) {
+        this.setState({ isLoggedin: Boolean });
+      }
+      console.log(res.data);
+      console.log(Boolean(res.data));
+    });
   }
 
   render() {
@@ -22,6 +37,7 @@ class App extends Component {
             <Route exact path="/" component={Landing} />
             <Route exact path="/emails" component={Dashboard} />
             <Route path="/emails/new" component={NewMail} />
+            <Route path="/practice" component={AuthExample} />
           </div>
         </BrowserRouter>
       </div>
---
+++ client/src/components/protectedroutePractice.js
@@ -0,0 +1,79 @@
+import React, { Component } from "react";
+import {
+  BrowserRouter as Router,
+  Route,
+  Link,
+  Redirect,
+  withRouter
+} from "react-router-dom";
+
+import axios from "axios";
+
+function AuthExample() {
+  isAuth();
+  return (
+    <Router>
+      <div>
+        <ul>
+          <li>
+            <Link to="/public">Public Page</Link>
+          </li>
+          <li>
+            <Link to="/protected">Protected Page</Link>
+          </li>
+        </ul>
+        <Route path="/public" component={Public} />
+
+        <PrivateRoute path="/protected" component={Protected} />
+      </div>
+    </Router>
+  );
+}
+
+const auth = {
+  isAuthenticated: false,
+  authenticate(cb) {
+    this.isAuthenticated = true;
+    setTimeout(cb, 100); // fake async
+  },
+  signout(cb) {
+    this.isAuthenticated = false;
+    setTimeout(cb, 100);
+  }
+};
+
+const isAuth = () => {
+  axios.get("/api/current_user").then(res => {
+    auth.isAuthenticated = Boolean(res.data);
+  });
+};
+
+function PrivateRoute({ component: Component, ...rest }) {
+  return (
+    <Route
+      {...rest}
+      render={props =>
+        auth.isAuthenticated ? (
+          <Component {...props} />
+        ) : (
+          <Redirect
+            to={{
+              pathname: "/login",
+              state: { from: props.location }
+            }}
+          />
+        )
+      }
+    />
+  );
+}
+
+function Public() {
+  return <h3>Public</h3>;
+}
+
+function Protected() {
+  return <h3>Protected</h3>;
+}
+
+export default AuthExample;
